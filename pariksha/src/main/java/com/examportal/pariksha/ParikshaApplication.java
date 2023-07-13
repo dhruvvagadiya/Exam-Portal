38744fc0245686d0dddc8f475a879a7dcf185a06
Dhruv Vagadiya <vagadiyadhruv00@gmail.com>
2023-07-13 11:39:08 +0530

    Initial Project Setup

---
+++ pariksha/src/main/java/com/examportal/pariksha/ParikshaApplication.java
@@ -0,0 +1,13 @@
+package com.examportal.pariksha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParikshaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParikshaApplication.class, args);
+	}
+
+}
---
+++ pariksha/src/test/java/com/examportal/pariksha/ParikshaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examportal.pariksha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParikshaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
